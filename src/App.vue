1820c2fda9175535d65e82b77ed4f102ccad9b6f
micasnaker <1186919723@qq.com>
2019-03-01 22:51:14 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- App -->
+<template>
+  <div id="app">
+
+    <!-- 底部 tabbar -->
+      <router-view></router-view>
+			<FooterGuid v-show="$route.meta.showFooter"></FooterGuid>
+  </div>
+</template>
+
+<script>
+import FooterGuid from './components/FooterGuid/FooterGuid'
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    FooterGuid
+  }
+}
+
+</script>
+
+<style lang="stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
